3e650b354e229a16a827deb9ae917c4abd52f9e1
fsfembuscado <fsfembuscado@addu.edu.ph>
2025-08-03 11:38:32 +0800

    Add files via upload

---
+++ todo_list/base/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+# from . import views
+from . views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegsiterPage
+
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+
+    path('login/', CustomLoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
+    path('register/', RegsiterPage.as_view(), name = 'register'),
+
+    path('', TaskList.as_view(), name = 'tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name = 'task'),  #task/<int:pk/ is a primary key
+    path('task-create/', TaskCreate.as_view(), name = 'task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name = 'task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name = 'task-delete'),
+    
+]
